02766ba361f25813ba464bc84d34b69af009b5fe
Talib Aliyev <133005678+alyvswag@users.noreply.github.com>
2026-09-21 22:52:54 +0400

    fix: Invalid refresh token change status

--- src/main/java/com/example/msauthkyc/exception/InvalidRefreshTokenException.java
+++ src/main/java/com/example/msauthkyc/exception/InvalidRefreshTokenException.java
@@ -1,9 +1,9 @@
 package com.example.msauthkyc.exception;
 
-import com.example.libexception.exception.UnauthorizedException;
+import com.example.libexception.exception.BadRequestException;
 import com.example.msauthkyc.exception.error.AuthErrorCode;
 
-public class InvalidRefreshTokenException extends UnauthorizedException {
+public class InvalidRefreshTokenException extends BadRequestException {
 
     public InvalidRefreshTokenException() {
         super(AuthErrorCode.INVALID_REFRESH_TOKEN);
